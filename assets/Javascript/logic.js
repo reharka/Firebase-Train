786c445d6b403e1109bcafccf77ecb5eff4b781b
Robyn Harka <reharka@gmail.com>
2017-11-10 17:28:23 -0500

    final update

---
+++ assets/Javascript/logic.js
@@ -0,0 +1,99 @@
+// Steps to complete:
+/*
+1. Create Firebase link
+2. Create button for adding new trains - then update the html + update the database
+3. Create a way to retrieve trains from the train database.
+4. Create a way to calculate the months worked. Using difference between start and current time. Then use moment.js formatting to set difference in months.
+5. Calculate Total billed
+
+
+*/
+// 1. Link to Firebase
+
+var config = {
+    apiKey: "AIzaSyBc-MDSwcnBvc54k8_Yc_fq5RTvtXqNPwo",
+    authDomain: "fir-train-28a24.firebaseapp.com",
+    databaseURL: "https://fir-train-28a24.firebaseio.com",
+    projectId: "fir-train-28a24",
+    storageBucket: "",
+    messagingSenderId: "255883908327"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var currentTime = moment();
+
+database.ref().on("child_added", function(childSnap) {
+
+    var name = childSnap.val().name;
+    var destination = childSnap.val().destination;
+    var firstTrain = childSnap.val().firstTrain;
+    var frequency = childSnap.val().frequency;
+    var min = childSnap.val().min;
+    var next = childSnap.val().next;
+
+    $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
+
+database.ref().on("value", function(snapshot) {
+   
+
+});
+
+//grabs information from the form
+$("#addTrainBtn").on("click", function() {
+
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = $("#firstInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+
+    //ensures that each input has a value
+    if (trainName == "") {
+        alert('Enter a train name.');
+        return false;
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
+        return false;
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
+        return false;
+    }
+
+    // MATH :(
+
+    //subtracts the first train time back a year to ensure it's before current time.
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+    // the time difference between current time and the first train
+    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        min: minUntilTrain,
+        next: nextTrain
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstInput").val("");
+    $("#frequencyInput").val("");
+
+    return false;
+});
